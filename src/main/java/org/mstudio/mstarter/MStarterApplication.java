378661f5bbd382739548401cbd7854580f96005f
Macrow <Macrow_wh@163.com>
2020-02-25 21:30:03 +0800

    first commit for MStarter

---
+++ src/main/java/org/mstudio/mstarter/MStarterApplication.java
@@ -0,0 +1,13 @@
+package org.mstudio.mstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MStarterApplication.class, args);
+	}
+
+}
